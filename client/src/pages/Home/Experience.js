44c20190a6909c988f9b363f74cef66de4e38f68
Akash-2009 <pandeyakashar098@gmail.com>
2024-03-16 11:29:16 +0530

    Update

--- client/src/pages/Home/Experience.js
+++ client/src/pages/Home/Experience.js
@@ -8,8 +8,8 @@ const Experience = () => {
   return (
     <div>
       <SectionTitle title="Expierences" />
-      <div className="flex py-10">
-        <div className="flex flex-col gap-10 border-l-2 border-[#136b468c]">
+      <div className="flex py-10 gap-20 sm:flex-col">
+        <div className="flex flex-col gap-10 border-l-2 border-[#136b468c] w-1/3 sm:flex-row sm:overflow-scroll sm:w-full">
           {Experiences.map((experience, index) => (
             <div
               onClick={() => {
@@ -21,7 +21,7 @@ const Experience = () => {
                 className={`text-xl px-10
                 ${
                   selectedItem === index
-                    ? "text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#2c80799d]"
+                    ? "text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#2c80799d] py-3"
                     : "text-white"
                 }`}
               >
@@ -30,7 +30,11 @@ const Experience = () => {
             </div>
           ))}
         </div>
-        <div></div>
+        <div flex  flex-col gap-5>
+          <h1 className="text-secondary text-2xl">{Experiences[selectedItem].title}</h1>
+          <h1 className="text-tertiary text-2xl">{Experiences[selectedItem].location}</h1>
+          <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
       </div>
     </div>
   );
